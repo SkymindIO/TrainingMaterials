091bb65769adc220a53bf13cca7b46853aaa8223
chiaweil <chiawei@skymind.io>
2018-11-27 01:17:50 +0900

    Added training

--- dl4j-labs/src/main/java/org/deeplearning4j/training/convolution/objectdetection/VideoObjectDetection.java
+++ dl4j-labs/src/main/java/org/deeplearning4j/training/convolution/objectdetection/VideoObjectDetection.java
@@ -2,21 +2,27 @@ package org.deeplearning4j.training.convolution.objectdetection;
 
 
 import org.datavec.image.loader.NativeImageLoader;
+import org.datavec.image.transform.ColorConversionTransform;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.layers.objdetect.DetectedObject;
+import org.deeplearning4j.nn.layers.objdetect.YoloUtils;
+import org.deeplearning4j.zoo.ZooModel;
 import org.deeplearning4j.zoo.model.TinyYOLO;
 import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacv.*;
+import org.deeplearning4j.zoo.util.darknet.VOCLabels;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+import org.nd4j.linalg.factory.Nd4j;
 
 import java.awt.event.KeyEvent;
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 import static org.bytedeco.javacpp.opencv_core.FONT_HERSHEY_DUPLEX;
 import static org.bytedeco.javacpp.opencv_imgproc.putText;
 import static org.bytedeco.javacpp.opencv_imgproc.rectangle;
+import static org.bytedeco.javacpp.opencv_imgproc.resize;
+import static org.opencv.imgproc.Imgproc.COLOR_BGR2RGB;
 
 /**
  * Yolo Detection with video
@@ -31,12 +37,18 @@ public class VideoObjectDetection
 {
     private static Thread thread;
 
+
+    private static final int gridWidth = 13;
+    private static final int gridHeight = 13;
+
     private static double detectionThreshold = 0.5;
+    private static final int tinyyolowidth = 416;
+    private static final int tinyyoloheight = 416;
 
 
     public static void main(String[] args) throws Exception {
 
-        String videoPath = "//Users//wei//Desktop//Skymind//Videos//videoSample.mp4";
+        String videoPath = "videoSample.mp4";
         FrameGrabber grabber = FrameGrabber.createDefault(videoPath);
         OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
 
@@ -44,82 +56,74 @@ public class VideoObjectDetection
 
         String winName = "Object Detection";
         CanvasFrame canvas = new CanvasFrame(winName);
-        canvas.setCanvasSize(grabber.getImageWidth(), grabber.getImageHeight());
 
-        ComputationGraph model = (ComputationGraph)TinyYOLO.builder().build().initPretrained();
+        int w = grabber.getImageWidth();
+        int h = grabber.getImageHeight();
 
-        org.deeplearning4j.nn.layers.objdetect.Yolo2OutputLayer yout =
-                (org.deeplearning4j.nn.layers.objdetect.Yolo2OutputLayer)model.getOutputLayer(0);
 
-        NativeImageLoader loader = new NativeImageLoader();
+        canvas.setCanvasSize(w, h);
+
+        ZooModel model = TinyYOLO.builder().numClasses(0).build();
+        ComputationGraph initializedModel = (ComputationGraph) model.initPretrained();
+
+
+        NativeImageLoader loader = new NativeImageLoader(tinyyolowidth, tinyyoloheight, 3, new ColorConversionTransform(COLOR_BGR2RGB));
+        ImagePreProcessingScaler scaler = new ImagePreProcessingScaler(0, 1);
+        VOCLabels labels = new VOCLabels();
+
+        System.out.println("Start running video");
 
         while (true)
         {
-            try {
-                Frame frame = grabber.grab();
+            Frame frame = grabber.grab();
 
-                opencv_core.Mat matFrame = converter.convert(frame);
-
-                //if a thread is null, create new thread
-                if (thread == null)
+            //if a thread is null, create new thread
+            if (thread == null)
+            {
+                thread = new Thread(() ->
                 {
-                    thread = new Thread(() ->
+                    while (frame != null)
                     {
-                        while (frame != null)
+                        try
                         {
-                            INDArray inputArr = null;
+                            opencv_core.Mat rawImage = converter.convert(frame);
+                            opencv_core.Mat resizeImage = new opencv_core.Mat();
+                            resize(rawImage, resizeImage, new opencv_core.Size(tinyyolowidth, tinyyoloheight));
 
-                            try
-                            {
-                                inputArr = loader.asRowVector(matFrame);
+                            INDArray inputImage = loader.asMatrix(resizeImage);
+                            scaler.transform(inputImage);
+                            INDArray outputs = initializedModel.outputSingle(inputImage);
+                            List<DetectedObject> objs = YoloUtils.getPredictedObjects(Nd4j.create(((TinyYOLO) model).getPriorBoxes()), outputs, detectionThreshold, 0.4);
+
+
+                            for (DetectedObject obj : objs) {
+                                double[] xy1 = obj.getTopLeftXY();
+                                double[] xy2 = obj.getBottomRightXY();
+                                String label = labels.getLabel(obj.getPredictedClass());
+                                int x1 = (int) Math.round(w * xy1[0] / gridWidth);
+                                int y1 = (int) Math.round(h * xy1[1] / gridHeight);
+                                int x2 = (int) Math.round(w * xy2[0] / gridWidth);
+                                int y2 = (int) Math.round(h * xy2[1] / gridHeight);
+                                rectangle(rawImage, new opencv_core.Point(x1, y1), new opencv_core.Point(x2, y2), opencv_core.Scalar.RED, 2, 0, 0);
+                                putText(rawImage, label, new opencv_core.Point(x1 + 2, y2 - 2), FONT_HERSHEY_DUPLEX, 1, opencv_core.Scalar.GREEN);
                             }
-                            catch(IOException e)
-                            {
 
-                            }
-
-
+                            canvas.showImage(converter.convert(rawImage));
 
                         }
-                    });
-                    thread.start();
-                }
-
-                //continous get the max suppression of all the threads and display it.
-                //TinyYoloPrediction.getINSTANCE().markWithBoundingBox(matFrame, frame.imageWidth, frame.imageHeight, false);
-
-
-
-                /*
-                INDArray results = model.outputSingle(inputArr);
-                List<DetectedObject> objs = yout.getPredictedObjects(results, detectionThreshold);
-
-                for (DetectedObject obj : objs) {
-                    double[] xy1 = obj.getTopLeftXY();
-                    double[] xy2 = obj.getBottomRightXY();
-                    String label = labels.get(obj.getPredictedClass());
-                    int x1 = (int) Math.round(w * xy1[0] / gridWidth);
-                    int y1 = (int) Math.round(h * xy1[1] / gridHeight);
-                    int x2 = (int) Math.round(w * xy2[0] / gridWidth);
-                    int y2 = (int) Math.round(h * xy2[1] / gridHeight);
-                    rectangle(image, new opencv_core.Point(x1, y1), new opencv_core.Point(x2, y2), opencv_core.Scalar.RED);
-                    putText(image, label, new opencv_core.Point(x1 + 2, y2 - 2), FONT_HERSHEY_DUPLEX, 1, opencv_core.Scalar.GREEN);
-                }
-                canvas.setTitle(new File(metadata.getURI()).getName() + " - HouseNumberDetection");
-                canvas.setCanvasSize(w, h);
-                canvas.showImage(converter.convert(image));
-
-                canvas.showImage(converter.convert(matFrame));
-
-                KeyEvent t = canvas.waitKey(25);
-
-                if ((t != null) && (t.getKeyCode() == KeyEvent.VK_Q)) {
-                    break;
-                }
-                */
+                        catch (Exception e)
+                        {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
+                thread.start();
             }
-            catch(FrameGrabber.Exception e)
-            {
+
+
+            KeyEvent t = canvas.waitKey(33);
+
+            if ((t != null) && (t.getKeyCode() == KeyEvent.VK_Q)) {
                 break;
             }
         }
